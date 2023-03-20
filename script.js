7c574abbd70bd4c21e0da59884f25382b4443cbb
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-20 11:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 // Your Script here.
+
+var lookup = {
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
+};
+
+
+function rot13(encodedstr){
+
+	var words=encodedstr.split(" ");
+	let decodedArr = [];
+
+	for(let i=0;i<words.length;i++){
+		let word=words[i];
+		let decoded_word="";
+		
+		for(let j=0;j<word.length;j++){
+			var decoded_char=lookup[word.charAt(j)];
+			decoded_word+=decoded_char;
+		}
+		decodedArr.push(decoded_word);
+	}
+
+	return decodedArr.join(" ");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = rot13;
